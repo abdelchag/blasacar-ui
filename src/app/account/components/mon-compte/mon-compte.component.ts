836992ff80e037e4031f089d1a0fb178bcc22485
Misbahi Mohamed <misbahi.med@gmail.com>
2021-09-26 20:39:24 +0200

    filtre travel details reserve

--- src/app/account/components/mon-compte/mon-compte.component.ts
+++ src/app/account/components/mon-compte/mon-compte.component.ts
@@ -1,13 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { SocialUser } from 'angularx-social-login';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { Subject } from 'rxjs';
-import { ROUTING_PATH } from 'src/app/routing-constants';
-import { StringUtils } from 'src/utils/string-utils';
-import { ActionCodeEnum, ACTION_CONNEXION_LIST, ConnexionAction } from '../../models/connexion-action';
-import { FaceInfoSuppPopupComponent } from '../face-info-supp-popup/face-info-supp-popup.component';
-import { FacebookService } from '../services/facebook.service';
+import { ConnexionAction } from '../../models/connexion-action';
 
 @Component({
   selector: 'blasacar-mon-compte',
@@ -24,10 +18,7 @@ export class MonCompteComponent implements OnInit {
   origin: string;
 
   constructor(
-    private readonly modalRef: BsModalRef,
-    private readonly modalService: BsModalService,
-    private readonly facebookService: FacebookService,
-    private readonly router: Router
+    private readonly modalRef: BsModalRef
   ) {
   }
 
@@ -37,56 +28,6 @@ export class MonCompteComponent implements OnInit {
   closeModal(onlyClose: boolean = false): void {
     this.hideEvent.next(onlyClose);
     this.modalRef.hide();
-  }
-
-  public launchFacebookAction(): void {
-    switch (this.action.actionCode) {
-      case ActionCodeEnum.CONNEXION:
-        this.facebookService.facebookConnexion()
-          .subscribe(() => this.closeModal());
-        break;
-      case ActionCodeEnum.INSCRIPTION:
-        this.facebookService.getFacebookUser().subscribe(user => {
-          this.openFaceInfoSupp(user);
-          this.closeModal();
-        });
-        break;
-    }
-  }
-
-  public launchEmailAction(): void {
-    switch (this.action.actionCode) {
-      case ActionCodeEnum.CONNEXION:
-        this.router.navigate([ROUTING_PATH.CONNEXION_MEMBER],
-          {
-            queryParams: {
-              origin: this.origin
-            }
-          });
-        break;
-      case ActionCodeEnum.INSCRIPTION:
-        this.router.navigate([ROUTING_PATH.MEMBER]);
-        break;
-    }
-    this.modalRef.hide();
-  }
-
-  public switchAction(): void {
-    this.action = ACTION_CONNEXION_LIST.find(a => StringUtils.equals(a.actionCode.toString(), this.action.switchAction.toString()));
-  }
-
-  private openFaceInfoSupp(socialUser: SocialUser): void {
-    this.modalService.show(
-      FaceInfoSuppPopupComponent, {
-      animated: true,
-      initialState: {
-        user: {
-          ...socialUser,
-          birthDate: null,
-          gender: null
-        },
-      }
-    });
   }
 
 }
